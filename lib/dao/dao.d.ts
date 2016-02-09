c4c685f7f0a66df3182d4b7c720d560e2f0696b3
bruno <bvilhena@northplains.com>
2016-02-09 17:11:40 +0000

    removing lib again

--- lib/dao/dao.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../model/model.d.ts" />
-declare module DAO {
-    interface DAO<T extends Model.Identifiable> {
-        create(t: T): T;
-        read(id: number): T;
-        update(t: T): boolean;
-        delete(id: number): boolean;
-    }
-}
