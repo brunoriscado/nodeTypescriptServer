e72c9f5e870e870ce73c8f54d9e8d958fa0165a9
bruno <bvilhena@northplains.com>
2016-02-09 15:37:05 +0000

    changes to tsconfig.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var exec = require('child_process').exec;
 var sourcemaps = require('gulp-sourcemaps');
 var typescript = require('gulp-tsc');
 var tsProject = ts.createProject('tsconfig.json');
 var del = require('del');
+// var jasmine = require('gulp-jasmine');
 var forever = require('forever-monitor');
+var path = require('path');
 
 function errorLog (error) {
   	console.error.bind(error);
@@ -13,13 +16,22 @@ function errorLog (error) {
 
 //The --sourcemap option is used to generate a .js.map file.
 //It is used by debuggers to map the generated JavaScript to the source TypeScript.
-gulp.task('typescript-compile', function() {
-  	return tsProject.src(['src/ts/**/*.ts'])
-		.pipe(ts(tsProject))
-    	.pipe(sourcemaps.init())
-    	.pipe(sourcemaps.write("maps")) //fix the mapping folder structure
-    	.on('error', errorLog)
-    	.pipe(gulp.dest('js')); //fix the dest folder structure
+// gulp.task('typescript-compile', function() {
+//   	return tsProject.src([path.join('.', 'src', 'ts', '**', '*.ts')])
+// 		.pipe(ts(tsProject))
+//     	.pipe(sourcemaps.init())
+//     	.pipe(sourcemaps.write("maps")) //fix the mapping folder structure
+//     	.on('error', errorLog)
+//     	.pipe(gulp.dest('js')); //fix the dest folder structure
+// });
+
+
+gulp.task('typescript-compile',  function (cb) {
+  exec('tsc', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
 
 // watch the files for changes and rebuild everything
@@ -28,11 +40,16 @@ gulp.task('watch', function () {
 });
 
 gulp.task('clean', function() {
-    del.sync(['js/**/*.js', 'maps/**/*.map', '!maps', '!js']);
+    del.sync(['lib/**/*.js', 'lib/**/*.ts', 'lib/**/*.map', 'lib/**', '!lib']);
 });
 
 gulp.task('run', function () {
-    new forever.Monitor('js/src/server-test.js').start();
+    new forever.Monitor('lib/server-test.js').start();
 });
 
-gulp.task('default', ['typescript-compile', 'watch']);
+gulp.task('default', ['clean', 'typescript-compile', 'watch']);
+
+// gulp.task('test', 'Runs the Jasmine test specs', ['typescript-compile'], function () {
+//   return gulp.src('test/*.js')
+//     .pipe(jasmine());
+// });
